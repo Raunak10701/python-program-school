92f8ae10074627acc33a4db759a6b385f62eae5d
Raunak10701 <raunaktripathi107@gmail.com>
2026-07-04 22:55:39 +0530

    Add files via upload

---
+++ comparision between 2 numbers.py
@@ -0,0 +1,10 @@
+while True:
+    #while true is used to run code in intrective mode for multiple times
+ a=int(input("enter first number"))
+ b=int(input("enter second number"))
+ if a>b:
+    print(a,"is greater than",b)
+ elif b>a:
+    print(b,"is greater than",a)
+ else:
+    print("the given numbers are equal")
