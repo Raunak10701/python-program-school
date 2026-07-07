7de38a47cd8e07abd54de890cee3c9b247022d47
Raunak10701 <raunaktripathi107@gmail.com>
2026-07-07 12:51:42 +0530

    Add files via upload

---
+++ voting right.py
@@ -0,0 +1,9 @@
+while True:
+ a=eval(input("enter your age"))
+ n=input("enter your nationality")
+ if a>=18 and n=='indian':
+    print("you are eligible for voting")
+ else:
+    print("you are not eligible for voting")
+ print("in",n)
+ 
