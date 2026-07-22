c5b12d8b7879704151ce7e84c1f3677601797c94
Raunak10701 <raunaktripathi107@gmail.com>
2026-07-22 16:56:50 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,7 @@
+p=eval(input(" enter principle amount"))
+r=eval(input("enter rate of interest"))
+t=eval(input("enter time "))
+si=(p*r*t)/100
+a=p+si
+print("SI will be",si )
+print("amount will be",a)
