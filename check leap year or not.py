26e741fcec436582a9f4e27e7294182f7860de5c
Raunak <raunaktripathi107@gmail.com>
2026-07-26 19:47:21 +0530

    Add files via upload

---
+++ check leap year or not.py
@@ -0,0 +1,6 @@
+while True:
+ y=int(input("enter year"))
+ if (y%4==0 and y%100!=0)or(y%400==0):
+     print(y,"is a leap year")
+ else:
+     print(y,"is not a leap year")
---
+++ check odd or even.py
@@ -0,0 +1,7 @@
+while True:
+ a= int(input("enter a number :"))
+ if (a%2==0):
+    print("yes",a,"is an even number")
+ else:
+    print(a,"is an odd number")
+
